1615129960688f881850df4244c7c501bcb4b37f
apinto5 <54562918+apinto5@users.noreply.github.com>
2020-04-21 17:09:35 -0400

    Update and rename php/signup.php to js/login_scripts.js
    PHP doesn't work on static pages; therefor had to switch to JS implementation. Working on getting this working.

--- php/signup.php
+++
@@ -1,9 +0,0 @@
-<?php
- $path = 'login/info.txt';
- if (isset($_POST['loginUser']) && isset($_POST['loginPass'])) {
-     $fh = fopen($path,"a+");
-     $string = $_POST['loginUser'].' - '.$_POST['loginPass'];
-     fwrite($fh,$string); // Write information to the file
-     fclose($fh); // Close the file
- }
-?>
